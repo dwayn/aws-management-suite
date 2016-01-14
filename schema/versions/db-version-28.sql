dfc29f6cf00b0cd5957ec076b6037fdf251491d8
Dwayn Matthies <dwayn.matthies@gmail.com>
2016-01-14 11:11:06 -0800

    Update the column sizes for EC2/EBS identifiers to prepare for these changes: https://aws.amazon.com/blogs/aws/heads-up-longer-ec2-ebs-resource-ids-coming-in-2016/?sc_channel=em&sc_campaign=ec2longerids_2016&sc_publisher=aws&sc_medium=em_9207&sc_content=other&sc_country=global&sc_geo=global&sc_category=ec2&mkt_tok=3RkMMJWWfF9wsRovuazOZKXonjHpfsX66OUuX662lMI%2F0ER3fOvrPUfGjI4DScNkI%2BSLDwEYGJlv6SgFS7HHMbR617gKXRc%3D

---
+++ schema/versions/db-version-28.sql
@@ -0,0 +1,41 @@
+ALTER TABLE `ami_block_devices` MODIFY COLUMN `snapshot_id` VARCHAR(25) DEFAULT NULL; 
+
+--
+
+ALTER TABLE `deleted_snapshots` MODIFY COLUMN `snapshot_id` VARCHAR(25) NOT NULL;
+
+--
+
+ALTER TABLE `deleted_snapshots` MODIFY COLUMN `volume_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `deleted_volumes` MODIFY COLUMN `volume_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `elastic_ips` MODIFY COLUMN `instance_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `host_volumes` MODIFY COLUMN `instance_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `hosts` MODIFY COLUMN `instance_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `security_group_associations` MODIFY COLUMN `instance_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `snapshot_schedules` MODIFY COLUMN `instance_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `snapshots` MODIFY COLUMN `snapshot_id` VARCHAR(25) DEFAULT NULL;
+
+--
+
+ALTER TABLE `volumes` MODIFY COLUMN `volume_id` VARCHAR(25) DEFAULT NULL;
